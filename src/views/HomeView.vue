66f889bdfb23a8845c1e0510337cad41c03a0fe5
K_Richard <119009903+DrDevoo@users.noreply.github.com>
2023-09-10 12:15:53 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    HelloWorld
+    <RouterLink to="/teszt"><h3>masik</h3></RouterLink>
+  </main>
+</template>
